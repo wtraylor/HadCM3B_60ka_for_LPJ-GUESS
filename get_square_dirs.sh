72231f7fa4216d73f211eef63c24d05fa51b4d93
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-06-18 15:59:19 +0200

    Remove debug message

--- get_square_dirs.sh
+++ get_square_dirs.sh
@@ -4,8 +4,6 @@
 #
 # Author: Wolfgang Traylor (wolfgang.traylor@senckenberg.de)
 
-echo SQUARE_SIZE: $SQUARE_SIZE >&2
-
 ./get_square_regions.py | while read square; do
   east=$(echo "$square" | cut --field=1 --delimiter=' ')
   west=$(echo "$square" | cut --field=2 --delimiter=' ')
