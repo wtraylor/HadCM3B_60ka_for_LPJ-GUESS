3d96fedeb5cad80811a40b90b9f932415c6f9af3
Wolfgang Traylor <wolfgang.traylor@posteo.de>
2019-06-18 15:46:07 +0200

    Qualify the square directory with all coordinates
    This way we will be able to extract the coordinates from the file path.

---
+++ get_square_dirs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Get the directory names for all the square subregions.
+# The directories are identified by the south-east corner of the square.
+#
+# Author: Wolfgang Traylor (wolfgang.traylor@senckenberg.de)
+
+echo SQUARE_SIZE: $SQUARE_SIZE >&2
+
+./get_square_regions.py | while read square; do
+  east=$(echo "$square" | cut --field=1 --delimiter=' ')
+  west=$(echo "$square" | cut --field=2 --delimiter=' ')
+  south=$(echo "$square" | cut --field=3 --delimiter=' ')
+  north=$(echo "$square" | cut --field=4 --delimiter=' ')
+  echo "${east}_${west}_${south}_${north}"
+done
